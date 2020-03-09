b15de2b3fdfabfbd7f5d852e9fd67cf43d720898
rajeshvarada <rajesh.varada@aa.com>
2020-03-09 16:45:59 -0500

    updated

--- src/app/pnr-creation/pnr-creation.component.ts
+++ src/app/pnr-creation/pnr-creation.component.ts
@@ -2,8 +2,8 @@ import {Reservation} from '../model/reservation';
 import {Component, OnInit} from '@angular/core';
 import {PnrCreationService} from './pnrCreation.service';
 import {HttpErrorResponse} from '@angular/common/http';
-import {FormControl} from "@angular/forms";
-import {Observable} from "rxjs";
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 
 @Component({
@@ -40,22 +40,24 @@ export class PnrCreationComponent implements OnInit {
             );
     }
 
-    private _filter(value: string): string[] {
-        const filterValue = value.toLowerCase();
-        return this.options.filter(option => option.toLowerCase().includes(filterValue));
-    }
-
-    // this.reservation.tripType = 'RoundTrip';
     public tripTypeSelected() {
         this.reservation.tripType === 'OneWay' ? this.returnHidden = true : this.returnHidden = false;
     }
 
+    public onChangeDeparture(dep: Date): void {
+        this.reservation.departureDate = (dep.getMonth() + 1) + '/' + dep.getDate() + '/' + dep.getFullYear();
+    }
+
+    public onChangeReturn(returnDate: Date): void {
+        this.reservation.returnDate = (returnDate.getMonth() + 1) + '/' + returnDate.getDate() + '/' + returnDate.getFullYear();
+    }
+
     public submitData() {
         console.log(this.reservation.firstName);
-        const dep = this.reservation.departureDateUI;
-        const returnDate = this.reservation.returnDateUI;
-        this.reservation.departureDate = dep.getMonth() + '/' + dep.getDate() + '/' + dep.getFullYear();
-        this.reservation.returnDate = returnDate.getMonth() + '/' + returnDate.getDate() + '/' + returnDate.getFullYear();
+        // const dep = this.reservation.departureDateUI;
+        // const returnDate = this.reservation.returnDateUI;
+        // this.reservation.departureDate = dep.getMonth() + '/' + dep.getDate() + '/' + dep.getFullYear();
+        // this.reservation.returnDate = returnDate.getMonth() + '/' + returnDate.getDate() + '/' + returnDate.getFullYear();
         const spitted = this.reservation.frequentFlyerNumber.split('-');
         console.log(spitted);
         this.reservation.frequentFlyerNumber = spitted[0].trim();
@@ -81,5 +83,10 @@ export class PnrCreationComponent implements OnInit {
             url = url + '/reservation/view/find-your-trip?firstName=' + this.reservation.firstName + '&lastName=' + this.reservation.lastName + '&recordLocator=' + this.pnr;
             window.open('https://' + url, '_blank');
         }
+    }
+
+    private _filter(value: string): string[] {
+        const filterValue = value.toLowerCase();
+        return this.options.filter(option => option.toLowerCase().includes(filterValue));
     }
 }
